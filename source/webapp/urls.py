a1240fc2c231cbabae4418b30156140b362a8823
Karybek_Zholbunov <userbek85@gmail.com>
2024-06-24 15:01:36 +0600

    переделал от вывод списка статьей и создание статьи
    теперь все работает с баззой данных

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from webapp.views import index, create_article
+from webapp.views import index, create_article, article_detail
 
 urlpatterns = [
     path('', index),
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,35 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from webapp.article_db import ArticleDb
+from webapp.models import Article
 
 
 # Create your views here.
 
 
 def index(request):
-    print(request.GET.getlist("my_params"))
-    context = {
-        "name": "John",
-        "age": 20,
-        "articles": ArticleDb.articles
-
-    }
-    return render(request, "index.html", context=context)
+    articles = Article.objects.order_by("-created_at")
+    return render(request, "index.html", context={"articles": articles})
 
 
 def create_article(request):
     if request.method == "GET":
         return render(request, "create_article.html")
     else:
-        title = request.POST.get("title")
-        content = request.POST.get("content")
-        author = request.POST.get("author")
-        ArticleDb.articles.append(
-            {
-                "title": title,
-                "content": content,
-                "author": author
-            }
+        Article.objects.create(
+            title=request.POST.get("title"),
+            content=request.POST.get("content"),
+            author=request.POST.get("author")
         )
         return HttpResponseRedirect("/")
         # return render(request, "article.html", context={
