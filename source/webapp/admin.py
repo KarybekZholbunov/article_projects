129f2503ccc36d2e8f8f901dfcd602378e64a97f
Karybek_Zholbunov <userbek85@gmail.com>
2024-06-24 13:32:54 +0600

    Создал модель Article,  зарегистрировал его в админке

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from webapp.models import Article
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'author', 'created_at']
+    list_display_links = ['id', 'title']
+    list_filter = ['author']
+    search_fields = ['title', 'content']
+    fields = ['title', 'author', 'content', 'created_at', 'update_at']
+    readonly_fields = ['created_at', 'update_at']
+
+
 # Register your models here.
+admin.site.register(Article, ArticleAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=100, null=False, blank=False, verbose_name="Название")
+    author = models.CharField(max_length=50, null=False, blank=False, verbose_name="Автор", default="Неизвестный")
+    content = models.TextField(max_length=1000, null=False, blank=False, verbose_name="Контент")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+    update_at = models.DateTimeField(auto_now=True, verbose_name="Время изменения")
+
+    def __str__(self):
+        return f"{self.pk}. {self.title}: {self.author}"
+
+
+    class Meta:
+        db_table = "articles"
+        verbose_name = "Статья"
+        verbose_name_plural = "Статьи"
