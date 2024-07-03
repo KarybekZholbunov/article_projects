16eaaf45e20e22d38116b3eb76f487236cf97150
Karybek_Zholbunov <userbek85@gmail.com>
2024-07-03 10:11:12 +0600

    Добавил в модель новое поле статус

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+statuses = [("new", "Новая"), ("moderated", "Модерированная"), ("deleted", "Удаленная")]
+
 
 class Article(models.Model):
     title = models.CharField(max_length=100, null=False, blank=False, verbose_name="Название")
@@ -7,9 +9,11 @@ class Article(models.Model):
     content = models.TextField(max_length=1000, null=False, blank=False, verbose_name="Контент")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
     update_at = models.DateTimeField(auto_now=True, verbose_name="Время изменения")
+    status = models.CharField(max_length=25, choices=statuses, verbose_name="Статус", default=statuses[0][0])
 
     def __str__(self):
         return f"{self.pk}. {self.title}: {self.author}"
+
 
 
     class Meta:
